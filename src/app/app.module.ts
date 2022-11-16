3d8f95750901ac1d43eee6ad0e0678defe8d5f44
joaquinlaracastillo <joaquin.lara.castillo.18@gmail.com>
2022-11-16 00:22:16 -0300

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, isPlatform } from '@ionic/angular';
 import {Camera} from '@ionic-native/camera/ngx';
 
 import { AppComponent } from './app.component';
@@ -17,12 +17,28 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 import { LoginPage } from './pages/login/login.page';
 import { QRCodeModule } from 'angularx-qrcode';
 
+const getConfig = () => {
+  if (isPlatform('ios')) {
+    return {
+      backButtonText: 'Volver',
+      backButtonIcon: "ios-back-arrow",
+      pageTransition: 'ios-transition',
+    }
+  }
+
+  return {
+    backButtonText: 'Volver',
+      backButtonIcon: "close",
+      pageTransition: 'ios-transition',
+  }
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     QRCodeModule,
     BrowserModule, 
-    IonicModule.forRoot(),
+    IonicModule.forRoot(getConfig()),
     AppRoutingModule,
     ComponentsModule,
     HttpClientModule,
