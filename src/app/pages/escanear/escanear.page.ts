6bff40d0d777f5417e51ff686e89d1f381f23c09
joaquinlaracastillo <joaquin.lara.castillo.18@gmail.com>
2022-11-29 22:00:17 -0300

    ultimos detalles entrega 3

--- src/app/pages/escanear/escanear.page.ts
+++ src/app/pages/escanear/escanear.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { AlertController } from '@ionic/angular';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { ApiService } from 'src/app/services/api.service';
+import { StorageTestService } from 'src/app/services/storage-test.service';
+
 
 @Component({
   selector: 'app-escanear',
@@ -9,7 +17,31 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 export class EscanearPage implements OnInit {
   scannedResult: any
 
-  constructor(private barcodeScanner: BarcodeScanner) { }
+  //Correo de usuario
+  userLoginData: string = localStorage.getItem('correo')
+
+  //Data usuario API
+  usuarioDataHtml: Usuario;
+
+  //Lista de observables para evitar la fuga de memoria generada por las mutliples subscripciones
+  listObservables: Array<Subscription>;
+  
+  constructor(private barcodeScanner: BarcodeScanner, private alertController: AlertController, private api: ApiService) { 
+	this.getUsuarioByCorreo(this.userLoginData)
+    console.log("correo: " + this.userLoginData)
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Guardado',
+	  subHeader: this.scannedResult ,
+      message:  'Correo enviado a: ' + this.usuarioDataHtml.correo ,
+
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
   ngOnInit(): void {
 		this.barcodeScanner
@@ -23,5 +55,20 @@ export class EscanearPage implements OnInit {
 				console.log('Error', err)
 			})
 	}
+	getUsuarioByCorreo(correo){
+		const load1$ = this.api.getUsuarios().subscribe((data) => {
+		  for(let i = 0; i < data.length; i++){
+			if(correo == data[i].correo) {
+			  this.listObservables = [load1$];
+			  this.usuarioDataHtml = data[i]      
+		  }     
+			else {
+			  continue
+			}
+		  }
+		});
+	  }
+
+	
 
 }
